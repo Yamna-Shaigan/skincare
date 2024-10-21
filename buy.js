9763d7d130679ee70b5068df043185efc347fb8c
Yamna-Shaigan <RUBINASHAMIM05@GMAIL.COM>
2024-10-21 14:48:41 -0700

    firstcommit

---
+++ buy.js
@@ -0,0 +1,5 @@
+document.getElementById('buyNowBtn').addEventListener('click', function() {
+    const quantity = document.getElementById('quantity').value;
+    alert(`Thank you for your purchase! You have ordered ${quantity} item(s).`);
+});
+
